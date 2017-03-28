16f92d2bc35aa6e72de24886cf91d367d109f2af
Jhon <jhonperez.j95@gmail.com>
2017-03-28 03:25:41 -0500

    Aplicando algo de Bootstrap

--- MiTD/settings.py
+++ MiTD/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'MiTD.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Static/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,5 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 LOGIN_REDIRECT_URL = reverse_lazy('torneos:torneo_listar')
